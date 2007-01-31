40a5024524c2bed80753e5ef936f3b1bf473161d
ross <ross@b067294f-1dea-0310-9683-c47a78595994>
2007-01-31 19:21:20 +0000

    Be a desktop by default
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-desktop-2@1458 b067294f-1dea-0310-9683-c47a78595994

--- main.c
+++ main.c
@@ -69,11 +69,11 @@ main (int argc, char **argv)
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   g_signal_connect (window, "delete-event", G_CALLBACK (gtk_main_quit), NULL);
   gtk_widget_set_name(window, "TakuWindow");
-#if AS_DESKTOP
+#if STANDALONE
+  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
+#else
   gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DESKTOP);
   gtk_window_fullscreen (GTK_WINDOW (window));
-#else
-  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
 #endif
   gtk_widget_show (window);
 
